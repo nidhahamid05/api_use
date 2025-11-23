bbc511aa9cf494695f4a82940bdf941e7ef1c3e1
nidhahamid05 <nidhahamid05@gmail.com>
2025-11-23 13:08:08 +0530

    getCharacters component added

--- my-api/src/App.js
+++ my-api/src/App.js
@@ -1,32 +1,14 @@
-import { BASE_URL } from "./API/api-url";
 import "./App.css";
-import { useEffect, useState } from "react";
+import GetCharacters from "./components/getcharacters";
+
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    fetch(`${BASE_URL}/character`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCharacters(data.data);
-      });
-  }, []);
-
-  console.log("data from api", characters);
 
   return (
     <>
       <div>A Disney characters application</div>
-      <div>
-        {characters.map((character) => (
-          <>
-            <div> Character name: {character.name}</div>
-            <img alt="character" src={character.imageUrl} />
-            <div>Movies: {character.films}</div>
-          </>
-        ))}
-      </div>
+      <GetCharacters/>
+     
     </>
   );
 }
